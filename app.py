8b816f709207415175ad0a386a73a00b4a15c76d
roger <ionyshchenko@sequans.com>
2013-03-24 17:35:07 +0200

    i

--- app.py
+++ app.py
@@ -2,4 +2,5 @@ def application(env, s_res):
     s = '200 OK'
     res_headers = [('Content-type', 'text/plain')]
     s_res(s, res_headers)
-    return [str(env)]
+    #return [str(env)]
+    return ['Yo!!!']
