5380c225557c9a9573caad5559b94956844314fb
roger <ionyshchenko@sequans.com>
2013-03-24 17:44:10 +0200

    i

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-def application(env, s_res):
-    s = '200 OK'
-    res_headers = [('Content-type', 'text/plain')]
-    s_res(s, res_headers)
-    #return [str(env)]
-    return 'Yo!!!'
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type','text/html')])
+    return "Hello World"
